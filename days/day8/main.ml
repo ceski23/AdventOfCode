7890ab45136b1d86e6390ac45c33e195ec364ee5
Cezary Bober <ceski23@gmail.com>
2023-12-08 19:29:11 +0100

    feat: add day 8 part 2

--- days/day8/main.ml
+++ days/day8/main.ml
@@ -1,6 +1,9 @@
 open Core
 module NetworkMap = Map.Make (String)
 
+let rec gcd a b = if b = 0 then a else gcd b (a mod b)
+let lcm a b = a / gcd a b * b
+
 let parse_network_node line =
   let first_match =
     Re2.first_match_exn
@@ -30,57 +33,45 @@ let parse_input (input : string) =
 let rec traverse_network
   (directions : char array)
   (network : (string * string) NetworkMap.t)
-  (steps : int)
-  (index : int)
+  ?(steps : int = 0)
+  ?(index : int = 0)
+  (should_stop : string -> bool)
   (start_node : string)
   =
-  if String.( = ) start_node "ZZZ"
+  if should_stop start_node
   then steps
   else (
-    let new_node =
+    let next_node =
       match directions.(index) with
       | 'L' -> Map.find_exn network start_node |> fst
       | 'R' -> Map.find_exn network start_node |> snd
       | _ -> failwith "invalid data"
     in
     let next_index = (index + 1) mod Array.length directions in
-    traverse_network directions network (steps + 1) next_index new_node)
+    traverse_network
+      directions
+      network
+      ?steps:(Some (steps + 1))
+      ?index:(Some next_index)
+      should_stop
+      next_node)
 ;;
 
 let () =
   let directions, network = parse_input (In_channel.read_all "days/day8/input.txt") in
-  let result = traverse_network directions network 0 0 "AAA" in
+  let result = traverse_network directions network (String.equal "ZZZ") "AAA" in
   printf "PART1: %d\n" result;
   assert (result = 20513)
 ;;
 
-let rec traverse_network_as_ghost
-  (directions : char array)
-  (network : (string * string) NetworkMap.t)
-  (steps : int)
-  (index : int)
-  (start_nodes : string list)
-  =
-  if List.for_all start_nodes ~f:(String.is_suffix ~suffix:"Z")
-  then steps
-  else (
-    let new_nodes =
-      match directions.(index) with
-      | 'L' -> start_nodes |> List.map ~f:(fun node -> Map.find_exn network node |> fst)
-      | 'R' -> start_nodes |> List.map ~f:(fun node -> Map.find_exn network node |> snd)
-      | _ -> failwith "invalid data"
-    in
-    let next_index = (index + 1) mod Array.length directions in
-    if steps mod 1000000 = 0 then printf "STEPS: %d\n" steps;
-    Out_channel.flush stdout;
-    traverse_network_as_ghost directions network (steps + 1) next_index new_nodes)
-;;
-
 let () =
   let directions, network = parse_input (In_channel.read_all "days/day8/input.txt") in
-  let start_nodes =
-    Map.keys network |> List.filter ~f:(fun node -> String.is_suffix node ~suffix:"A")
+  let result =
+    Map.keys network
+    |> List.filter ~f:(fun node -> String.is_suffix node ~suffix:"A")
+    |> List.map ~f:(traverse_network directions network (String.is_suffix ~suffix:"Z"))
+    |> List.fold ~init:1 ~f:(fun acc length -> lcm acc length)
   in
-  let result = traverse_network_as_ghost directions network 0 0 start_nodes in
-  printf "PART2: %d\n" result
+  printf "PART2: %d\n" result;
+  assert (result = 15995167053923)
 ;;
