b0c26ab19be21eae4dc7cafb04b6011df41e5aa7
Cezary Bober <ceski23@gmail.com>
2023-12-08 15:45:41 +0100

    feat: finish day 7

--- days/day7/main.ml
+++ days/day7/main.ml
@@ -1,5 +1,4 @@
 open Core
-module CardsSet = Set.Make (Char)
 
 type hand =
   | FiveOfAKind of char list
@@ -11,27 +10,21 @@ type hand =
   | HighCard of char list
 
 let find_most_common_card (cards : char list) =
-  let card, _ =
-    cards
-    |> List.sort_and_group ~compare:compare_char
-    |> List.fold ~init:('X', 0) ~f:(fun (char, count) cards_group ->
-      let len = List.length cards_group in
-      let new_char = List.last_exn cards_group in
-      if count < len && Char.( <> ) new_char 'J' then new_char, len else char, count)
-  in
-  card
+  cards
+  |> List.sort_and_group ~compare:compare_char
+  |> List.fold ~init:('X', 0) ~f:(fun (char, count) cards_group ->
+    let len = List.length cards_group in
+    let new_char = List.last_exn cards_group in
+    if count < len && Char.( <> ) new_char 'J' then new_char, len else char, count)
+  |> fst
 ;;
 
 let parse_hand (replace_joker : bool) (hand : char list) =
   let cards_to_group =
     if replace_joker
-    then
-      hand
-      |> String.of_list
-      |> String.substr_replace_all
-           ~pattern:"J"
-           ~with_:(hand |> find_most_common_card |> String.of_char)
-      |> String.to_list
+    then (
+      let replacement = hand |> find_most_common_card in
+      hand |> List.map ~f:(fun card -> if Char.equal card 'J' then replacement else card))
     else hand
   in
   let groups = List.sort_and_group ~compare:compare_char cards_to_group in
@@ -63,26 +56,20 @@ let parse_input (replace_joker : bool) (input : string list) =
     | _ -> failwith "invalid data")
 ;;
 
-let compare_cards (a : char) (b : char) =
-  let cards_order =
-    [ 'A'; 'K'; 'Q'; 'J'; 'T'; '9'; '8'; '7'; '6'; '5'; '4'; '3'; '2' ] |> List.rev
-  in
-  let a_index, _ = List.findi_exn cards_order ~f:(fun _ x -> Char.equal x a) in
-  let b_index, _ = List.findi_exn cards_order ~f:(fun _ x -> Char.equal x b) in
-  Int.compare a_index b_index
-;;
-
-let rec compare_same_hands (a : char list) (b : char list) =
+let rec compare_same_types (cards_order : char list) (a : char list) (b : char list) =
   match a, b with
-  | ha :: ta, hb :: tb ->
-    if Char.equal ha hb then compare_same_hands ta tb else compare_cards ha hb
+  | head_a :: tail_a, head_b :: tail_b ->
+    if Char.equal head_a head_b
+    then compare_same_types cards_order tail_a tail_b
+    else (
+      let index_a, _ = List.findi_exn cards_order ~f:(fun _ x -> Char.equal x head_a) in
+      let index_b, _ = List.findi_exn cards_order ~f:(fun _ x -> Char.equal x head_b) in
+      Int.compare index_b index_a)
   | _, _ -> failwith "bad input"
 ;;
 
-let compare_hands_with_bid (a : hand * int) (b : hand * int) =
-  let hand_a, _ = a in
-  let hand_b, _ = b in
-  match hand_a, hand_b with
+let compare_hands_with_bid (cards_order : char list) (a : hand * int) (b : hand * int) =
+  match a |> fst, b |> fst with
   | ( FiveOfAKind _
     , (FourOFAKind _ | FullHouse _ | ThreeOfAKind _ | TwoPair _ | OnePair _ | HighCard _)
     )
@@ -90,50 +77,42 @@ let compare_hands_with_bid (a : hand * int) (b : hand * int) =
   | FullHouse _, (ThreeOfAKind _ | TwoPair _ | OnePair _ | HighCard _)
   | ThreeOfAKind _, (TwoPair _ | OnePair _ | HighCard _)
   | TwoPair _, (OnePair _ | HighCard _)
-  | OnePair _, HighCard _ -> 1
+  | OnePair _, HighCard _ -> -1
   | FiveOfAKind a, FiveOfAKind b
   | FourOFAKind a, FourOFAKind b
   | FullHouse a, FullHouse b
   | ThreeOfAKind a, ThreeOfAKind b
   | TwoPair a, TwoPair b
   | OnePair a, OnePair b
-  | HighCard a, HighCard b -> compare_same_hands a b
-  | _ -> -1
+  | HighCard a, HighCard b -> compare_same_types cards_order a b
+  | _ -> 1
+;;
+
+let calc_total_winnings hands =
+  List.foldi hands ~init:0 ~f:(fun index acc (_, bid) ->
+    acc + (bid * (List.length hands - index)))
 ;;
 
 let () =
-  let hands =
-    parse_input false (In_channel.read_lines "days/day7/test.txt")
-    |> List.sort ~compare:compare_hands_with_bid
-    |> List.rev
-  in
+  let cards_order = [ '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9'; 'T'; 'J'; 'Q'; 'K'; 'A' ] in
   let result =
-    List.foldi hands ~init:0 ~f:(fun index acc (_, bid) ->
-      let rank = List.length hands - index in
-      (* (match hand with
-         | FiveOfAKind _ -> printf "FiveOFAKind %d * %d\n" bid rank
-         | FourOFAKind _ -> printf "FourOFAKind %d * %d\n" bid rank
-         | FullHouse _ -> printf "FullHouse %d * %d\n" bid rank
-         | ThreeOfAKind _ -> printf "ThreeOfAKind %d * %d\n" bid rank
-         | TwoPair _ -> printf "TwoPair %d * %d\n" bid rank
-         | OnePair _ -> printf "OnePair %d * %d\n" bid rank
-         | HighCard _ -> printf "HighCard %d * %d\n" bid rank); *)
-      acc + (bid * rank))
+    In_channel.read_lines "days/day7/input.txt"
+    |> parse_input false
+    |> List.sort ~compare:(compare_hands_with_bid cards_order)
+    |> calc_total_winnings
   in
-  printf "PART1: %d\n" result
+  printf "PART1: %d\n" result;
+  assert (result = 250370104)
 ;;
 
-(* assert (result = 250370104) *)
-
 let () =
-  let hands =
-    parse_input true (In_channel.read_lines "days/day7/test.txt")
-    |> List.sort ~compare:compare_hands_with_bid
-    |> List.rev
-  in
+  let cards_order = [ 'J'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9'; 'T'; 'Q'; 'K'; 'A' ] in
   let result =
-    List.foldi hands ~init:0 ~f:(fun index acc (_, bid) ->
-      acc + ((bid * List.length hands) - index))
+    In_channel.read_lines "days/day7/input.txt"
+    |> parse_input true
+    |> List.sort ~compare:(compare_hands_with_bid cards_order)
+    |> calc_total_winnings
   in
-  printf "PART2: %d\n" result
+  printf "PART2: %d\n" result;
+  assert (result = 251735672)
 ;;
