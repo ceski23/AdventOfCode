fff2d67a7eec0947b48dc9d7f77e52c254619e4b
Cezary Bober <ceski23@gmail.com>
2024-12-03 19:30:33 +0000

    chore: improve day3 code

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,52 +1,45 @@
 use anyhow::Result;
 use regex::Regex;
 
-fn calc_instructions(text: String) -> Result<u32> {
-    let real_mul_pattern = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
-    let sum = real_mul_pattern
+fn sum_uncorrupted_instructions(text: String) -> Result<u32> {
+    let instruction_pattern = Regex::new(r"mul\((?<x>\d{1,3}),(?<y>\d{1,3})\)")?;
+    let sum = instruction_pattern
         .captures_iter(&text)
         .filter_map(|capture| {
-            let (_, [a, b]) = capture.extract();
+            let x = &capture["x"].parse::<u32>().ok()?;
+            let y = &capture["y"].parse::<u32>().ok()?;
 
-            Some((a.parse::<u32>().ok()?, b.parse::<u32>().ok()?))
+            Some(x * y)
         })
-        .map(|(a, b)| a * b)
-        .sum::<u32>();
+        .sum();
 
     Ok(sum)
 }
 
-fn calc_instructions_conditionally(text: String) -> Result<u32> {
-    let real_mul_pattern = Regex::new(r"(?:mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))")?;
-    let sum = real_mul_pattern
+fn sum_uncorrupted_instructions_conditionally(text: String) -> Result<u32> {
+    let instruction_pattern = Regex::new(
+        r"(?:mul\((?<x>\d{1,3}),(?<y>\d{1,3})\))|(?<enable>do\(\))|(?<disable>don't\(\))",
+    )?;
+    let sum = instruction_pattern
         .captures_iter(&text)
-        .fold((true, 0), |(should_parse, sum), capture| {
-            if capture.get(3).is_some() {
+        .fold((true, 0), |(should_parse, sum), groups| {
+            if groups.name("enable").is_some() {
                 return (true, sum);
             }
 
-            if capture.get(4).is_some() {
+            if groups.name("disable").is_some() {
                 return (false, sum);
             }
 
-            if should_parse {
-                let n1 = capture
-                    .get(1)
-                    .unwrap()
-                    .as_str()
-                    .parse::<u32>()
-                    .unwrap_or_default();
-                let n2 = capture
-                    .get(2)
-                    .unwrap()
-                    .as_str()
-                    .parse::<u32>()
-                    .unwrap_or_default();
+            match (should_parse, groups.name("x"), groups.name("y")) {
+                (true, Some(x_match), Some(y_match)) => {
+                    let x = x_match.as_str().parse::<u32>().unwrap_or_default();
+                    let y = y_match.as_str().parse::<u32>().unwrap_or_default();
 
-                return (should_parse, sum + (n1 * n2));
+                    (should_parse, sum + (x * y))
+                }
+                _ => (should_parse, sum),
             }
-
-            (should_parse, sum)
         })
         .1;
 
@@ -61,7 +54,7 @@ mod tests {
     #[test]
     fn part1_test() -> Result<()> {
         let input = fs::read_to_string("inputs/day3_test.txt")?;
-        assert_eq!(calc_instructions(input)?, 161);
+        assert_eq!(sum_uncorrupted_instructions(input)?, 161);
 
         Ok(())
     }
@@ -69,7 +62,7 @@ mod tests {
     #[test]
     fn part1() -> Result<()> {
         let input = fs::read_to_string("inputs/day3.txt")?;
-        assert_eq!(calc_instructions(input)?, 173517243);
+        assert_eq!(sum_uncorrupted_instructions(input)?, 173517243);
 
         Ok(())
     }
@@ -77,7 +70,7 @@ mod tests {
     #[test]
     fn part2_test() -> Result<()> {
         let input = fs::read_to_string("inputs/day3_test2.txt")?;
-        assert_eq!(calc_instructions_conditionally(input)?, 48);
+        assert_eq!(sum_uncorrupted_instructions_conditionally(input)?, 48);
 
         Ok(())
     }
@@ -85,7 +78,10 @@ mod tests {
     #[test]
     fn part2() -> Result<()> {
         let input = fs::read_to_string("inputs/day3.txt")?;
-        assert_eq!(calc_instructions_conditionally(input)?, 100450138);
+        assert_eq!(
+            sum_uncorrupted_instructions_conditionally(input)?,
+            100450138
+        );
 
         Ok(())
     }
