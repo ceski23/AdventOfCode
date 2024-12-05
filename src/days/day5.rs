f9fb0c601b0c83599e1af04366f7c7bc9fbd1c1f
Cezary Bober <ceski23@gmail.com>
2024-12-05 21:24:36 +0000

    feat: day 5 part 1

---
+++ src/days/day5.rs
@@ -0,0 +1,85 @@
+use anyhow::{Ok, Result};
+
+type Pairs = Vec<(u32, u32)>;
+type UpdatesList = Vec<Vec<u32>>;
+
+fn parse_pairs(text: &str) -> Pairs {
+    text.lines()
+        .filter_map(|line| {
+            let (a, b) = line.split_once("|")?;
+            let a_num = a.parse::<u32>().ok()?;
+            let b_num = b.parse::<u32>().ok()?;
+
+            Some((a_num, b_num))
+        })
+        .collect()
+}
+
+fn parse_updates_list(text: &str) -> UpdatesList {
+    text.lines()
+        .map(|line| {
+            line.split(",")
+                .filter_map(|item| item.parse::<u32>().ok())
+                .collect()
+        })
+        .collect()
+}
+
+fn parse_input(input: String) -> Result<(Pairs, UpdatesList)> {
+    let (pairs, updates_list) = input
+        .split_once("\n\n")
+        .ok_or(anyhow::anyhow!("Invalid input"))?;
+
+    Ok((parse_pairs(pairs), parse_updates_list(updates_list)))
+}
+
+fn get_middle_page(update: &[u32]) -> u32 {
+    update[update.len().div_floor(2)]
+}
+
+fn sum_correctly_ordered_updates(pairs: Pairs, updates_list: UpdatesList) -> u32 {
+    pairs
+        .iter()
+        .fold(updates_list, |valid_updates, (a, b)| {
+            valid_updates
+                .iter()
+                .filter(|update| {
+                    let a_index = update.iter().position(|page| page == a);
+                    let b_index = update.iter().position(|page| page == b);
+
+                    match (a_index, b_index) {
+                        (Some(a_index), Some(b_index)) => a_index < b_index,
+                        _ => true,
+                    }
+                })
+                .cloned()
+                .collect()
+        })
+        .iter()
+        .map(|update| get_middle_page(update))
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn part1_test() -> Result<()> {
+        let input = fs::read_to_string("inputs/day5_test.txt")?;
+        let (pairs, updates) = parse_input(input)?;
+        assert_eq!(sum_correctly_ordered_updates(pairs, updates), 143);
+
+        Ok(())
+    }
+
+    #[test]
+    fn part1() -> Result<()> {
+        let input = fs::read_to_string("inputs/day5.txt")?;
+        let (pairs, updates) = parse_input(input)?;
+        assert_eq!(sum_correctly_ordered_updates(pairs, updates), 4135);
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(binary_heap_into_iter_sorted)]
+#![feature(int_roundings)]
 #![allow(dead_code)]
 mod days;
 
