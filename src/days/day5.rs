ec7bb72ffc41664ecbaf96fcddd2e1b06ffa3b1b
Cezary Bober <ceski23@gmail.com>
2024-12-05 22:43:34 +0000

    feat: day 5 part 2

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,4 +1,6 @@
 use anyhow::{Ok, Result};
+use itertools::Itertools;
+use std::collections::HashMap;
 
 type Pairs = Vec<(u32, u32)>;
 type UpdatesList = Vec<Vec<u32>>;
@@ -60,6 +62,39 @@ fn sum_correctly_ordered_updates(pairs: Pairs, updates_list: UpdatesList) -> u32
         .sum()
 }
 
+fn sum_fixed_updates(pairs: Pairs, updates_list: UpdatesList) -> u32 {
+    updates_list
+        .iter()
+        .filter_map(|updates| {
+            let mut counts_map = HashMap::<&u32, u32>::new();
+
+            pairs.iter().for_each(|(a, b)| {
+                if updates.contains(a) && updates.contains(b) {
+                    counts_map
+                        .entry(a)
+                        .and_modify(|count| *count += 1)
+                        .or_insert(1);
+                    counts_map.entry(b).or_default();
+                }
+            });
+
+            let ordered = counts_map
+                .iter()
+                .sorted_by_key(|(_, count)| **count)
+                .map(|(page, _)| **page)
+                .rev()
+                .collect::<Vec<u32>>();
+
+            if ordered == *updates {
+                None
+            } else {
+                Some(ordered)
+            }
+        })
+        .map(|update| get_middle_page(&update))
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,6 +114,24 @@ mod tests {
         let input = fs::read_to_string("inputs/day5.txt")?;
         let (pairs, updates) = parse_input(input)?;
         assert_eq!(sum_correctly_ordered_updates(pairs, updates), 4135);
+
+        Ok(())
+    }
+
+    #[test]
+    fn part2_test() -> Result<()> {
+        let input = fs::read_to_string("inputs/day5_test.txt")?;
+        let (pairs, updates) = parse_input(input)?;
+        assert_eq!(sum_fixed_updates(pairs, updates), 123);
+
+        Ok(())
+    }
+
+    #[test]
+    fn part2() -> Result<()> {
+        let input = fs::read_to_string("inputs/day5.txt")?;
+        let (pairs, updates) = parse_input(input)?;
+        assert_eq!(sum_fixed_updates(pairs, updates), 5285);
 
         Ok(())
     }
