98a228f13a96bf1678da9414909d1fce9c46d8eb
Cezary Bober <ceski23@gmail.com>
2025-12-11 16:57:12 +0000

    feat: day 11 part 2

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,9 +1,7 @@
-use pathfinding::prelude::count_paths;
 use std::collections::HashMap;
 
-fn count_signal_paths(text: String) -> usize {
-    let devices: HashMap<&str, Vec<String>> = text
-        .lines()
+fn parse_input(text: &str) -> HashMap<&str, Vec<String>> {
+    text.lines()
         .map(|line| {
             let (device_name, outputs_string) = line.split_once(": ").unwrap();
             let outputs = outputs_string
@@ -13,13 +11,57 @@ fn count_signal_paths(text: String) -> usize {
 
             (device_name, outputs)
         })
-        .collect();
+        .collect()
+}
 
-    count_paths(
-        "you".to_string(),
-        |current_node| devices.get(current_node.as_str()).unwrap().iter().cloned(),
-        |current_node| current_node == "out",
-    )
+fn dfs_memo(
+    src: &str,
+    dst: &str,
+    graph: &HashMap<&str, Vec<String>>,
+    memo: &mut HashMap<(String, String), usize>,
+) -> usize {
+    if src == dst {
+        return 1;
+    }
+
+    let key = (src.to_string(), dst.to_string());
+
+    if let Some(&count) = memo.get(&key) {
+        return count;
+    }
+
+    let mut result = 0;
+
+    if let Some(neighbours) = graph.get(src) {
+        for neighbour in neighbours {
+            result += dfs_memo(neighbour.as_str(), dst, graph, memo);
+        }
+    }
+
+    memo.insert(key, result);
+    result
+}
+
+fn count_signal_paths(text: String) -> usize {
+    let graph = parse_input(&text);
+    let mut memo = HashMap::new();
+
+    dfs_memo("you", "out", &graph, &mut memo)
+}
+
+fn find_signal_paths(text: String) -> usize {
+    let graph = parse_input(&text);
+    let mut memo = HashMap::new();
+
+    let svr_dac_fft_out = dfs_memo("svr", "dac", &graph, &mut memo)
+        * dfs_memo("dac", "fft", &graph, &mut memo)
+        * dfs_memo("fft", "out", &graph, &mut memo);
+
+    let svr_fft_dac_out = dfs_memo("svr", "fft", &graph, &mut memo)
+        * dfs_memo("fft", "dac", &graph, &mut memo)
+        * dfs_memo("dac", "out", &graph, &mut memo);
+
+    svr_dac_fft_out + svr_fft_dac_out
 }
 
 #[cfg(test)]
@@ -40,6 +82,22 @@ mod tests {
     fn part1() -> Result<()> {
         let input = fs::read_to_string("inputs/day11.txt")?;
         assert_eq!(count_signal_paths(input), 690);
+
+        Ok(())
+    }
+
+    #[test]
+    fn part2_test() -> Result<()> {
+        let input = fs::read_to_string("inputs/day11_test2.txt")?;
+        assert_eq!(find_signal_paths(input), 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn part2() -> Result<()> {
+        let input = fs::read_to_string("inputs/day11.txt")?;
+        assert_eq!(find_signal_paths(input), 557332758684000);
 
         Ok(())
     }
